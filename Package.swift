28aeb2395f7a5457a0a0351557b9ed4b1e4c9fea
Anka <runner@Mac-1719866440770.local>
2024-07-01 20:53:02 +0000

    v1.0.24

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "fluentAdFlowAdsWidget", url: "https://mobile-sdk.adflow-prod.minionplatform.com/ios/v1.0.24/FluentAdFlowAdsWidget.xcframework.v1.0.24.zip", checksum: "9301a1f6ec8a1ca5d030d2866219e385bbcdfef6395a2acfc2bc1fc8b5cd2ef8")
+        .binaryTarget(name: "fluentAdFlowAdsWidget", url: "https://mobile-sdk.adflow-prod.minionplatform.com/ios/v1.0.24/FluentAdFlowAdsWidget.xcframework.v1.0.24.zip", checksum: "a61d9cdb665c96f11713b8393f8da7aabf5fcd760dee144939ae8e89f99ecb5b")
     ]
 )
